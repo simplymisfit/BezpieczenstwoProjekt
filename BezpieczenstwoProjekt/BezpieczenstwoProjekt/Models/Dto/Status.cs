cfee109f0c19a56b17c42db7827b13fb504223ae
Filip Rosochacki <63207978+simplymisfit@users.noreply.github.com>
2023-05-16 17:11:38 +0200

    Created status dto

---
+++ BezpieczenstwoProjekt/BezpieczenstwoProjekt/Models/Dto/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace BezpieczenstwoProjekt.Models.Dto
+{
+    public class Status
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}

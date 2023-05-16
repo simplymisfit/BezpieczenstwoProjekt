16f9751cccddee6c21ca10ecb91225e6f84e91e6
Filip Rosochacki <63207978+simplymisfit@users.noreply.github.com>
2023-05-16 17:32:46 +0200

    Initial user authentication

--- BezpieczenstwoProjekt/BezpieczenstwoProjekt/Repositories/Implementation/UserAuthenticationService.cs
+++ BezpieczenstwoProjekt/BezpieczenstwoProjekt/Repositories/Implementation/UserAuthenticationService.cs
@@ -1,23 +1,67 @@
-﻿using BezpieczenstwoProjekt.Models.Dto;
+﻿using BezpieczenstwoProjekt.Models.Domain;
+using BezpieczenstwoProjekt.Models.Dto;
 using BezpieczenstwoProjekt.Repositories.Abstract;
+using Microsoft.AspNetCore.Identity;
 
 namespace BezpieczenstwoProjekt.Repositories.Implementation
 {
     public class UserAuthenticationService : IUserAuthenticationService
     {
-        public Task<Status> LoginAsync(Login model)
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserAuthenticationService(
+            SignInManager<ApplicationUser> signInManager, 
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<Status> LoginAsync(Login model)
         {
             throw new NotImplementedException();
         }
 
-        public Task LogoutAsync()
+        public async Task LogoutAsync()
         {
             throw new NotImplementedException();
         }
 
-        public Task<Status> RegisterAsync(Registration model)
+        public async Task<Status> RegisterAsync(Registration model)
         {
-            throw new NotImplementedException();
+            var status = new Status();
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+
+            if (userExists != null)
+            {
+                status.StatusCode = 0;
+                status.StatusMessage = "User already exists!";
+                return status;
+            }
+
+            ApplicationUser user = new ApplicationUser
+            {
+                SecurityStamp = Guid.NewGuid().ToString(),
+                Name = model.Name,
+                Email = model.Email,
+                UserName = model.Username,
+                EmailConfirmed = true,
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                status.StatusCode = 0;
+                status.StatusMessage = "User creation failed!";
+                return status;
+            }
+
+            //Role management
         }
     }
 }

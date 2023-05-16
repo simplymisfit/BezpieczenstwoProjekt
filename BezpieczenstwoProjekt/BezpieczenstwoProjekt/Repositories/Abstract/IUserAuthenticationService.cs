53ea38d478d2b9eed879acb05af3beedf70d6f41
Filip Rosochacki <63207978+simplymisfit@users.noreply.github.com>
2023-05-16 17:21:11 +0200

    WIP

--- BezpieczenstwoProjekt/BezpieczenstwoProjekt/Repositories/Abstract/IUserAuthenticationService.cs
+++ BezpieczenstwoProjekt/BezpieczenstwoProjekt/Repositories/Abstract/IUserAuthenticationService.cs
@@ -1,6 +1,11 @@
-﻿namespace BezpieczenstwoProjekt.Repositories.Abstract
+﻿using BezpieczenstwoProjekt.Models.Dto;
+
+namespace BezpieczenstwoProjekt.Repositories.Abstract
 {
     public interface IUserAuthenticationService
     {
+        Task<Status> LoginAsync(Login model);
+        Task<Status> RegisterAsync(Registration model);
+        Task LogoutAsync();
     }
 }
--- BezpieczenstwoProjekt/BezpieczenstwoProjekt/Repositories/Implementation/UserAuthenticationService.cs
+++ BezpieczenstwoProjekt/BezpieczenstwoProjekt/Repositories/Implementation/UserAuthenticationService.cs
@@ -1,8 +1,23 @@
-﻿using BezpieczenstwoProjekt.Repositories.Abstract;
+﻿using BezpieczenstwoProjekt.Models.Dto;
+using BezpieczenstwoProjekt.Repositories.Abstract;
 
 namespace BezpieczenstwoProjekt.Repositories.Implementation
 {
     public class UserAuthenticationService : IUserAuthenticationService
     {
+        public Task<Status> LoginAsync(Login model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LogoutAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Status> RegisterAsync(Registration model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
